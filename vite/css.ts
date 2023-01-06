d15d452212083be92b4846d535067d11391dc770
Niko <Niko.yuan@hupun.com>
2023-01-06 09:18:19 +0800

    chore: 删除

--- vite/css.ts
+++
@@ -1,9 +0,0 @@
-import type { CSSOptions } from 'vite'
-const css = {
-  preprocessorOptions: {
-    scss: {
-      additionalData: '@import "@/styles/element-plus/index.scss";',
-    },
-  },
-} as CSSOptions
-export default css
